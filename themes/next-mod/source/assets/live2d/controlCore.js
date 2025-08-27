dd2b5bfc11229b8689384c96ef1e843bf6c0fc23
5dbwat4 <me@5dbwat4.top>
2025-08-28 02:53:58 +0800

    Remove l2d, Add comfirmation, Modify post.

--- themes/next-mod/source/assets/live2d/controlCore.js
+++
@@ -1,6 +0,0 @@
-L2Dwidget.init({
-    live2d_path:"/assets/live2d/nahita.model3.json",
-    waifuPath: "/assets/live2d/waifu-tips.json",
-    cdnPath: "/assets/live2d/",
-    tools: ["hitokoto", "asteroids", "switch-model", "photo", "quit"]
-});
